5acea8e28352880b3195f9370497dcffdc5b52a4
Shouvik Chatterjee <58618873+techieshouvik@users.noreply.github.com>
2021-01-14 01:38:44 +0530

    Add files via upload

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,5 @@
 const preloaderContainer = document.querySelector('#preloader-container')
 
-document.body.style.overflow="hidden"
 setTimeout(function(){
-    preloaderContainer.classList.add("fade-out");
+    preloaderContainer.style.display="none" 
 }, 2000);
