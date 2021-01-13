12d5d44807d721eb3f9f683a7d477a0ac5336ac0
Shouvik Chatterjee <58618873+techieshouvik@users.noreply.github.com>
2021-01-14 01:31:41 +0530

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -3,5 +3,4 @@ const preloaderContainer = document.querySelector('#preloader-container')
 document.body.style.overflow="hidden"
 setTimeout(function(){
     preloaderContainer.classList.add("fade-out");
-    preloaderContainer.style.display = "none";
 }, 2000);
